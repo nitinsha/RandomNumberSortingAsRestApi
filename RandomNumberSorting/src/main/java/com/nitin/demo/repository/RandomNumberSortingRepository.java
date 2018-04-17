de4bbbdd17b389b78b6a8dc8440e752e5a692e4a
Nitin Insan <nitinrpl@gmail.com>
2018-04-17 20:58:24 +0530

    delete folders

--- RandomNumberSorting/src/main/java/com/nitin/demo/repository/RandomNumberSortingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nitin.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.nitin.demo.dto.RandonNumberSortingDTO;
-
-public interface RandomNumberSortingRepository extends CrudRepository<RandonNumberSortingDTO, Integer> {
-
-}
